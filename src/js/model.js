ee31e21a3e868258fd7b43064d76953b9e72c9fa
mrcjssnr <mrcjssnr@gmail.com>
2024-03-18 17:13:16 +0800

    implementing bookmark pt 1

--- src/js/model.js
+++ src/js/model.js
@@ -9,6 +9,7 @@ export const state = {
     resultsPerPage: RES_PER_PAGE,
     page: 1,
   },
+  bookmarks: [],
 };
 
 export const loadRecipe = async function (id) {
@@ -26,6 +27,10 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
+
+    if (state.bookmarks.some(bookmark => bookmark.id === id))
+      state.recipe.bookmarked = true;
+    else state.recipe.bookmarked = false;
 
     console.log(state.recipe);
   } catch (err) {
@@ -48,6 +53,9 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url, // check};
       };
     });
+
+    // this resets the pagination to page 1 if you search for another recipe
+    state.search.page = 1;
   } catch (err) {
     console.error(`${err} 💥💥💥`);
     throw err;
@@ -69,4 +77,21 @@ export const updateServings = function (newServings) {
   });
 
   state.recipe.servings = newServings;
+};
+
+export const addBookmark = function (recipe) {
+  // Add Bookmark
+  state.bookmarks.push(recipe);
+
+  // Mark current recipe as bookmark
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+};
+
+export const deleteBookmark = function (id) {
+  // delete bookmark
+  const index = state.bookmarks.findIndex(el => el.id === id);
+  state.bookmarks.splice(index, 1);
+
+  // mark current recipe as NOT bookmarked
+  if (id === state.recipe.id) state.recipe.bookmarked = false;
 };
